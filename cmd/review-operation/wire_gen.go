50c9c45213440333d8326ccd7edf603f01241703
liyl54 <liyl54@lenovo.com>
2024-03-18 16:27:34 +0800

    1. 实现审核评论和审核申诉的核心功能

--- cmd/review-operation/wire_gen.go
+++ cmd/review-operation/wire_gen.go
@@ -7,29 +7,34 @@
 package main
 
 import (
+	"github.com/go-kratos/kratos/v2"
+	"github.com/go-kratos/kratos/v2/log"
 	"review-operation/internal/biz"
 	"review-operation/internal/conf"
 	"review-operation/internal/data"
 	"review-operation/internal/server"
 	"review-operation/internal/service"
+)
 
-	"github.com/go-kratos/kratos/v2"
-	"github.com/go-kratos/kratos/v2/log"
+import (
+	_ "go.uber.org/automaxprocs"
 )
 
 // Injectors from wire.go:
 
 // wireApp init kratos application.
-func wireApp(confServer *conf.Server, confData *conf.Data, logger log.Logger) (*kratos.App, func(), error) {
-	dataData, cleanup, err := data.NewData(confData, logger)
+func wireApp(confServer *conf.Server, registry *conf.Registry, confData *conf.Data, logger log.Logger) (*kratos.App, func(), error) {
+	discovery := data.NewDiscovery(registry)
+	reviewClient := data.NewReviewServiceClient(discovery)
+	dataData, cleanup, err := data.NewData(confData, reviewClient, logger)
 	if err != nil {
 		return nil, nil, err
 	}
-	greeterRepo := data.NewGreeterRepo(dataData, logger)
-	greeterUsecase := biz.NewGreeterUsecase(greeterRepo, logger)
-	greeterService := service.NewGreeterService(greeterUsecase)
-	grpcServer := server.NewGRPCServer(confServer, greeterService, logger)
-	httpServer := server.NewHTTPServer(confServer, greeterService, logger)
+	operationRepo := data.NewOperationRepo(dataData, logger)
+	operationUsecase := biz.NewOperationUsecase(operationRepo, logger)
+	operationService := service.NewOperationService(operationUsecase)
+	grpcServer := server.NewGRPCServer(confServer, operationService, logger)
+	httpServer := server.NewHTTPServer(confServer, operationService, logger)
 	app := newApp(logger, grpcServer, httpServer)
 	return app, func() {
 		cleanup()
--- internal/server/http.go
+++ internal/server/http.go
@@ -1,7 +1,7 @@
 package server
 
 import (
-	v1 "review-operation/api/helloworld/v1"
+	v1 "review-operation/api/operation/v1"
 	"review-operation/internal/conf"
 	"review-operation/internal/service"
 
@@ -11,7 +11,7 @@ import (
 )
 
 // NewHTTPServer new an HTTP server.
-func NewHTTPServer(c *conf.Server, greeter *service.GreeterService, logger log.Logger) *http.Server {
+func NewHTTPServer(c *conf.Server, opSvc *service.OperationService, logger log.Logger) *http.Server {
 	var opts = []http.ServerOption{
 		http.Middleware(
 			recovery.Recovery(),
@@ -27,6 +27,6 @@ func NewHTTPServer(c *conf.Server, greeter *service.GreeterService, logger log.L
 		opts = append(opts, http.Timeout(c.Http.Timeout.AsDuration()))
 	}
 	srv := http.NewServer(opts...)
-	v1.RegisterGreeterHTTPServer(srv, greeter)
+	v1.RegisterOperationHTTPServer(srv, opSvc)
 	return srv
 }
